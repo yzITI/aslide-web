fb0e4e9cfdb154954d06363cd0528eb3b1374263
pikapikapikachu6 <939910620@qq.com>
2023-01-28 02:52:58 +1100

    entry page

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -1,6 +1,13 @@
 <script setup>
+let channel = $ref('')
 </script>
 
 <template>
-  <h1>This is entry page, viewer can type in channel id to join channel</h1>
+  <div class="m-auto min-h-screen flex flex-col justify-center items-center"> 
+    <h1 class="text-8xl uppercase font-[700] bg-gradient-to-r from-green-400 to-blue-500 inline-block bg-clip-text text-transparent">  Aslide </h1> 
+    <input ref="inputElement" class="w-1/5 mt-10 mb-4 rounded px-3 py-2 radius-2 border-2 border-gray-300 focus:ring-1 focus:border-blue-300 transition font-mono text-center"
+        :placeholder="channel"
+        :type="text"
+        v-model="channel">
+  </div>
 </template>
