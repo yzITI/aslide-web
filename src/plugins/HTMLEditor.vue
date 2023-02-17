48d5062263ea45b2d2d037bd4ca6c3d1057de6b2
VampireRabbit <1662025808@qq.com>
2023-02-17 12:21:34 +0800

    Update HTMLEditor.vue

--- src/plugins/HTMLEditor.vue
+++ src/plugins/HTMLEditor.vue
@@ -5,7 +5,7 @@ import { debounce } from '../utils/utils.js'
 import CodeMirror from '../components/CodeMirror.vue'
 const templates = {
   'Cover': '<div style="height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 2rem; text-align: center; box-sizing: border-box;">\n  <h1 style="font-size: 3rem;">Hello, world!</h1>\n  <p style="color: #555; font-size: 2rem;">This is an HTML slide!</p>\n</div>\n',
-  'Content': '<div style="height: 100%; display: flex; flex-direction: column; justify-content: space-around; padding: 4rem; box-sizing: border-box;">\n   <h1 style="font-size: 3rem;">Title</h1>\n  <p style="color: #555; font-size: 2rem;">1. First...</p>\n  <p style="color: #555; font-size: 2rem;">2. Second...</p>\n <p style="color: #555; font-size: 2rem;">3. Third...</p>\n</div>\n',
+  'Content': '<div style="height: 100%; display: flex; flex-direction: column; padding: 4rem; box-sizing: border-box;">\n  <h1 style="font-size: 3rem;">Title</h1>\n  <p style="color: #555; font-size: 2rem;">1. First...</p>\n  <p style="color: #555; font-size: 2rem;">2. Second...</p>\n  <p style="color: #555; font-size: 2rem;">3. Third...</p>\n</div>\n',
 }
 let html = $ref('')
 sendOut({ ready: 1 })
@@ -26,7 +26,7 @@ watch($$(html), debounce(v => {
     <h2 class="font-bold text-lg my-2">HTML Slide Editor</h2>
     <div class="flex items-center flex-wrap text-sm">
       Templates:
-      <button class="bg-blue-100 text-blue-500 font-bold px-2 rounded m-2" v-for="(v, t) in templates" @click="html = v">{{ t }}</button>
+      <button class="bg-blue-100 text-blue-500 font-bold px-2 rounded m-2 mr-0" v-for="(v, t) in templates" @click="html = v">{{ t }}</button>
     </div>
     <CodeMirror v-model="html" class="bg-white" />
   </div>
