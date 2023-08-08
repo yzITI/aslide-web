16be3b5f403f9af0995e157457fc6890d05a9407
Phantomlsh <phantomlsh@outlook.com>
2023-08-08 14:32:41 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,7 +19,7 @@ if (token) {
 }
 
 if (state.user?.token) router.push('/home')
-else window.location.href = 'https://cn.aauth.link/#/launch/aslide'
+else window.location.href = 'https://auth.njsc.ltd/#/launch/aslide'
 </script>
 
 <template>
