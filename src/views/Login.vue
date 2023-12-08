f97d9d38bd853611d12ff062c05d00f67f6d9981
root <root@localhost.localdomain>
2023-12-08 14:47:46 -0600

    Update user session storage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,10 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
-import { state, SS } from '../state'
+import { state, login } from '../state'
 const route = useRoute(), router = useRouter()
 const token = route.query.token
 
-function decode (input) {
-  if (!input) return {}
-  input = input.replace(/-/g, '+').replace(/_/g, '/')
-  const pad = input.length % 4
-  if (pad) input += new Array(5 - pad).join('=')
-  return JSON.parse(decodeURIComponent(atob(input).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join('')))
-}
-
-if (token) {
-  state.user = decode(token.split('.')[1])
-  state.user.token = token
-  SS.user = JSON.stringify(state.user)
-}
+if (token) login(token)
 
 if (state.user?.token) router.push('/home')
 else window.location.href = 'https://auth.njsc.ltd/#/launch/aslide'
